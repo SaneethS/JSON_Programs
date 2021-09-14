c26364b42b608387ded06430052b813e4e62b50b
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-09-14 19:07:06 +0530

    problem-6 is done where Stack is created for stock symbol purchased and sold

---
+++ src/com/stockstack/StockInterface.java
@@ -0,0 +1,13 @@
+package com.stockstack;
+
+public interface StockInterface {
+	double valueOf();
+	
+	void buy(int amount,String symbol);
+	
+	void sell(int amount,String symbol);
+	
+	void save(String filename);
+	
+	void printReport();
+}
